cb5edc6efda44650a020c74c40ff586f4906fe1e
Shinuk Yi <wook3024@gmail.com>
2022-01-05 01:21:46 +0900

    Update

--- dump_qa.py
+++ dump_qa.py
@@ -1,4 +1,4 @@
-import tensorflow_hub as hub
+# import tensorflow_hub as hub
 import numpy as np
 from tqdm import tqdm
 import pandas as pd
@@ -8,24 +8,19 @@ import config
 
 
 def process_qa():
-    df = pd.read_csv("data/COVID-QA.csv")
-    df.dropna(inplace=True, subset=["Answers", "Question"])
+    df = pd.read_csv("ratings_train.txt", sep="\t", quoting=3)
+    df.dropna(inplace=True, subset=["id", "document"])
 
     print("\nIndexing QA's...")
     for _, row in tqdm(df.iterrows()):
         # Index each qa pair along with the question id and embedding
-        insert_qa({
-            'question': row['Question'],
-            'answer': row['Answers'],
-            'question_vec': np.asarray(model([row['Question']])[0]).tolist(),
-            'q_id': row['Question ID']
-        })
+        insert_qa({"id": row["id"], "document": row["document"]})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load the universal-sentence-encoder model
-    model = hub.load(config.MODEL_URL)
-    print("Model loaded successfully...")
+    # model = hub.load(config.MODEL_URL)
+    # print("Model loaded successfully...")
     # Connect to elasticsearch node
     connect_elastic(config.ELASTIC_IP, config.ELASTIC_PORT)
     # Index the dataset
--- elastic.py
+++ elastic.py
@@ -18,65 +18,78 @@ def connect_elastic(ip, port):
 def create_qa_index():
     # Define the index mapping
     index_body = {
+        "settings": {
+            "number_of_shards": 8,
+            "number_of_replicas": 1,
+            "analysis": {
+                "analyzer": {
+                    "standard_analyzer": {"tokenizer": "standard"},
+                    "ngram_analyzer": {"type": "custom", "tokenizer": "my_ngram"},
+                    "korean_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "nori_tokenizer",
+                    },
+                },
+                "tokenizer": {
+                    "my_ngram": {
+                        "type": "ngram",
+                        "min_gram": 2,
+                        "max_gram": 2,
+                        "token_chars": ["letter", "digit"],
+                    },
+                    "nori_user_dict": {
+                        "type": "nori_tokenizer",
+                        "decompound_mode": "mixed",
+                    },
+                },
+            },
+        },
         "mappings": {
             "properties": {
-                "question": {
-                    "type": "text"
-                },
-                "answer": {
-                    "type": "text"
-                },
-                "question_vec": {
-                    "type": "dense_vector",
-                    "dims": 512
-                },
-                "q_id": {
-                    "type": "long"
-                }
+                "id": {"type": "long"},
+                "document": {"type": "text", "analyzer": "ngram_analyzer"},
             }
-        }
+        },
     }
     try:
         # Create the index if not exists
-        if not es_conn.indices.exists("covid-qa"):
+        if not es_conn.indices.exists("korea-qa"):
             # Ignore 400 means to ignore "Index Already Exist" error.
             es_conn.indices.create(
-                index="covid-qa", body=index_body  # ignore=[400, 404]
+                index="korea-qa", body=index_body  # ignore=[400, 404]
             )
-            print("Created Index -> covid-qa")
+            print("Created Index -> korea-qa")
         else:
-            print("Index covid-qa exists...")
+            print("Index korea-qa exists...")
     except Exception as ex:
         print(str(ex))
 
 
 def insert_qa(body):
-    if not es_conn.indices.exists("covid-qa"):
+    if not es_conn.indices.exists("korea-qa"):
         create_qa_index()
     # Insert a record into the es index
-    es_conn.index(index="covid-qa", body=body)
+    es_conn.index(index="korea-qa", body=body)
     # print("QA successfully inserted...")
 
 
 def semantic_search(query_vec, thresh=1.2, top_n=10):
     # Retrieve top_n semantically similar records for the given query vector
-    if not es_conn.indices.exists("covid-qa"):
+    if not es_conn.indices.exists("korea-qa"):
         return "No records found"
     s_body = {
         "query": {
             "script_score": {
-                "query": {
-                    "match_all": {}
-                },
+                "query": {"match_all": {}},
                 "script": {
                     "source": "cosineSimilarity(params.query_vector, 'question_vec') + 1.0",
-                    "params": {"query_vector": query_vec}
-                }
+                    "params": {"query_vector": query_vec},
+                },
             }
         }
     }
     # Semantic vector search with cosine similarity
-    result = es_conn.search(index="covid-qa", body=s_body)
+    result = es_conn.search(index="korea-qa", body=s_body)
     total_match = len(result["hits"]["hits"])
     print("Total Matches: ", str(total_match))
     # print(result)
@@ -84,27 +97,36 @@ def semantic_search(query_vec, thresh=1.2, top_n=10):
     if total_match > 0:
         q_ids = []
         for hit in result["hits"]["hits"]:
-            if hit['_score'] > thresh and hit['_source']['q_id'] not in q_ids and len(data) <= top_n:
-                print("--\nscore: {} \n question: {} \n answer: {}\n--".format(hit["_score"], hit["_source"]['question'], hit["_source"]['answer']))
-                q_ids.append(hit['_source']['q_id'])
-                data.append({'question': hit["_source"]['question'], 'answer': hit["_source"]['answer']})
+            if (
+                hit["_score"] > thresh
+                and hit["_source"]["q_id"] not in q_ids
+                and len(data) <= top_n
+            ):
+                print(
+                    "--\nscore: {} \n question: {} \n answer: {}\n--".format(
+                        hit["_score"],
+                        hit["_source"]["question"],
+                        hit["_source"]["answer"],
+                    )
+                )
+                q_ids.append(hit["_source"]["q_id"])
+                data.append(
+                    {
+                        "question": hit["_source"]["question"],
+                        "answer": hit["_source"]["answer"],
+                    }
+                )
     return data
 
 
 def keyword_search(query, thresh=1.2, top_n=10):
     # Retrieve top_n records using TF-IDF scoring for the given query vector
-    if not es_conn.indices.exists("covid-qa"):
+    if not es_conn.indices.exists("korea-qa"):
         return "No records found"
-    k_body = {
-        "query": {
-            "match": {
-                "question": query
-            }
-        }
-    }
+    k_body = {"query": {"match": {"question": query}}}
 
     # Keyword search
-    result = es_conn.search(index="covid-qa", body=k_body)
+    result = es_conn.search(index="korea-qa", body=k_body)
     total_match = len(result["hits"]["hits"])
     print("Total Matches: ", str(total_match))
     # print(result)
@@ -112,8 +134,23 @@ def keyword_search(query, thresh=1.2, top_n=10):
     if total_match > 0:
         q_ids = []
         for hit in result["hits"]["hits"]:
-            if hit['_score'] > thresh and hit['_source']['q_id'] not in q_ids and len(data) <= top_n:
-                print("--\nscore: {} \n question: {} \n answer: {}\n--".format(hit["_score"], hit["_source"]['question'], hit["_source"]['answer']))
-                q_ids.append(hit['_source']['q_id'])
-                data.append({'question': hit["_source"]['question'], 'answer': hit["_source"]['answer']})
+            if (
+                hit["_score"] > thresh
+                and hit["_source"]["q_id"] not in q_ids
+                and len(data) <= top_n
+            ):
+                print(
+                    "--\nscore: {} \n question: {} \n answer: {}\n--".format(
+                        hit["_score"],
+                        hit["_source"]["question"],
+                        hit["_source"]["answer"],
+                    )
+                )
+                q_ids.append(hit["_source"]["q_id"])
+                data.append(
+                    {
+                        "question": hit["_source"]["question"],
+                        "answer": hit["_source"]["answer"],
+                    }
+                )
     return data
